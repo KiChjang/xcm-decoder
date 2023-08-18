51a0832fcc1be9338314e5c60aeb519011360ae2
Keith Yeung <kungfukeith11@gmail.com>
2023-08-18 04:37:15 -0700

    Format all source files

--- src/main.rs
+++ src/main.rs
@@ -25,109 +25,115 @@ fn main() -> Result<(), String> {
 }
 
 fn print_v2_xcm(xcm: &xcm::v2::Xcm<()>) {
-    let xcm_str = xcm.0.iter().map(|inst| {
-        use xcm::v2::Instruction::*;
-        let inst_str = match inst {
-            WithdrawAsset(_) => "WithdrawAsset",
-            ReserveAssetDeposited(_) => "ReserveAssetDeposited",
-            ReceiveTeleportedAsset(_) => "ReceiveTeleportedAsset",
-            QueryResponse { .. } => "QueryResponse",
-            TransferAsset { .. } => "TransferAsset",
-            TransferReserveAsset { .. } => "TransferReserveAsset",
-            Transact { .. } => "Transact",
-            HrmpNewChannelOpenRequest { .. } => "HrmpNewChannelOpenRequest",
-            HrmpChannelAccepted { .. } => "HrmpChannelAccepted",
-            HrmpChannelClosing { .. } => "HrmpChannelClosing",
-            ClearOrigin => "ClearOrigin",
-            DescendOrigin(_) => "DescendOrigin",
-            ReportError { .. } => "ReportError",
-            DepositAsset { .. } => "DepositAsset",
-            DepositReserveAsset { .. } => "DepositReserveAsset",
-            ExchangeAsset { .. } => "ExchangeAsset",
-            InitiateReserveWithdraw { .. } => "InitiateReserveWithdraw",
-            InitiateTeleport { .. } => "InitiateTeleport",
-            QueryHolding { .. } => "QueryHolding",
-            BuyExecution { .. } => "BuyExecution",
-            RefundSurplus => "RefundSurplus",
-            SetErrorHandler(_) => "SetErrorHandler",
-            SetAppendix(_) => "SetAppendix",
-            ClearError => "ClearError",
-            ClaimAsset { .. } => "ClaimAsset",
-            Trap(_) => "Trap",
-            SubscribeVersion { .. } => "SubscribeVersion",
-            UnsubscribeVersion => "UnsubscribeVersion",
-        };
-        inst_str.to_string()
-    })
-    .fold("v2".to_string(), |mut acc, inst| {
-        acc.push(',');
-        acc.push_str(&inst);
-        acc
-    });
+    let xcm_str = xcm
+        .0
+        .iter()
+        .map(|inst| {
+            use xcm::v2::Instruction::*;
+            let inst_str = match inst {
+                WithdrawAsset(_) => "WithdrawAsset",
+                ReserveAssetDeposited(_) => "ReserveAssetDeposited",
+                ReceiveTeleportedAsset(_) => "ReceiveTeleportedAsset",
+                QueryResponse { .. } => "QueryResponse",
+                TransferAsset { .. } => "TransferAsset",
+                TransferReserveAsset { .. } => "TransferReserveAsset",
+                Transact { .. } => "Transact",
+                HrmpNewChannelOpenRequest { .. } => "HrmpNewChannelOpenRequest",
+                HrmpChannelAccepted { .. } => "HrmpChannelAccepted",
+                HrmpChannelClosing { .. } => "HrmpChannelClosing",
+                ClearOrigin => "ClearOrigin",
+                DescendOrigin(_) => "DescendOrigin",
+                ReportError { .. } => "ReportError",
+                DepositAsset { .. } => "DepositAsset",
+                DepositReserveAsset { .. } => "DepositReserveAsset",
+                ExchangeAsset { .. } => "ExchangeAsset",
+                InitiateReserveWithdraw { .. } => "InitiateReserveWithdraw",
+                InitiateTeleport { .. } => "InitiateTeleport",
+                QueryHolding { .. } => "QueryHolding",
+                BuyExecution { .. } => "BuyExecution",
+                RefundSurplus => "RefundSurplus",
+                SetErrorHandler(_) => "SetErrorHandler",
+                SetAppendix(_) => "SetAppendix",
+                ClearError => "ClearError",
+                ClaimAsset { .. } => "ClaimAsset",
+                Trap(_) => "Trap",
+                SubscribeVersion { .. } => "SubscribeVersion",
+                UnsubscribeVersion => "UnsubscribeVersion",
+            };
+            inst_str.to_string()
+        })
+        .fold("v2".to_string(), |mut acc, inst| {
+            acc.push(',');
+            acc.push_str(&inst);
+            acc
+        });
 
     println!("{}", xcm_str);
 }
 
 fn print_v3_xcm(xcm: &xcm::v3::Xcm<()>) {
-    let xcm_str = xcm.0.iter().map(|inst| {
-        use xcm::v3::Instruction::*;
-        let inst_str = match inst {
-            WithdrawAsset(_) => "WithdrawAsset",
-            ReserveAssetDeposited(_) => "ReserveAssetDeposited",
-            ReceiveTeleportedAsset(_) => "ReceiveTeleportedAsset",
-            QueryResponse { .. } => "QueryResponse",
-            TransferAsset { .. } => "TransferAsset",
-            TransferReserveAsset { .. } => "TransferReserveAsset",
-            Transact { .. } => "Transact",
-            HrmpNewChannelOpenRequest { .. } => "HrmpNewChannelOpenRequest",
-            HrmpChannelAccepted { .. } => "HrmpChannelAccepted",
-            HrmpChannelClosing { .. } => "HrmpChannelClosing",
-            ClearOrigin => "ClearOrigin",
-            DescendOrigin(_) => "DescendOrigin",
-            ReportError { .. } => "ReportError",
-            DepositAsset { .. } => "DepositAsset",
-            DepositReserveAsset { .. } => "DepositReserveAsset",
-            ExchangeAsset { .. } => "ExchangeAsset",
-            InitiateReserveWithdraw { .. } => "InitiateReserveWithdraw",
-            InitiateTeleport { .. } => "InitiateTeleport",
-            ReportHolding { .. } => "ReportHolding",
-            BuyExecution { .. } => "BuyExecution",
-            RefundSurplus => "RefundSurplus",
-            SetErrorHandler(_) => "SetErrorHandler",
-            SetAppendix(_) => "SetAppendix",
-            ClearError => "ClearError",
-            ClaimAsset { .. } => "ClaimAsset",
-            Trap(_) => "Trap",
-            SubscribeVersion { .. } => "SubscribeVersion",
-            UnsubscribeVersion => "UnsubscribeVersion",
-            BurnAsset(_) => "BurnAsset",
-            ExpectAsset(_) => "ExpectAsset",
-            ExpectOrigin(_) => "ExpectOrigin",
-            ExpectError(_) => "ExpectError",
-            ExpectTransactStatus(_) => "ExpectTransactStatus",
-            QueryPallet { .. } => "QueryPallet",
-            ExpectPallet { .. } => "ExpectPallet",
-            ReportTransactStatus(_) => "ReportTransactStatus",
-            ClearTransactStatus => "ClearTransactStatus",
-            UniversalOrigin(_) => "UniversalOrigin",
-            ExportMessage { .. } => "ExportMessage",
-            LockAsset { .. } => "LockAsset",
-            UnlockAsset { .. } => "UnlockAsset",
-            NoteUnlockable { .. } => "NoteUnlockable",
-            RequestUnlock { .. } => "RequestUnlock",
-            SetFeesMode { .. } => "SetFeesMode",
-            SetTopic { .. } => "SetTopic",
-            ClearTopic => "ClearTopic",
-            AliasOrigin(_) => "AliasOrigin",
-            UnpaidExecution { .. } => "UnpaidExecution",
-        };
-        inst_str.to_string()
-    })
-    .fold("v2:".to_string(), |mut acc, inst| {
-        acc.push(',');
-        acc.push_str(&inst);
-        acc
-    });
+    let xcm_str = xcm
+        .0
+        .iter()
+        .map(|inst| {
+            use xcm::v3::Instruction::*;
+            let inst_str = match inst {
+                WithdrawAsset(_) => "WithdrawAsset",
+                ReserveAssetDeposited(_) => "ReserveAssetDeposited",
+                ReceiveTeleportedAsset(_) => "ReceiveTeleportedAsset",
+                QueryResponse { .. } => "QueryResponse",
+                TransferAsset { .. } => "TransferAsset",
+                TransferReserveAsset { .. } => "TransferReserveAsset",
+                Transact { .. } => "Transact",
+                HrmpNewChannelOpenRequest { .. } => "HrmpNewChannelOpenRequest",
+                HrmpChannelAccepted { .. } => "HrmpChannelAccepted",
+                HrmpChannelClosing { .. } => "HrmpChannelClosing",
+                ClearOrigin => "ClearOrigin",
+                DescendOrigin(_) => "DescendOrigin",
+                ReportError { .. } => "ReportError",
+                DepositAsset { .. } => "DepositAsset",
+                DepositReserveAsset { .. } => "DepositReserveAsset",
+                ExchangeAsset { .. } => "ExchangeAsset",
+                InitiateReserveWithdraw { .. } => "InitiateReserveWithdraw",
+                InitiateTeleport { .. } => "InitiateTeleport",
+                ReportHolding { .. } => "ReportHolding",
+                BuyExecution { .. } => "BuyExecution",
+                RefundSurplus => "RefundSurplus",
+                SetErrorHandler(_) => "SetErrorHandler",
+                SetAppendix(_) => "SetAppendix",
+                ClearError => "ClearError",
+                ClaimAsset { .. } => "ClaimAsset",
+                Trap(_) => "Trap",
+                SubscribeVersion { .. } => "SubscribeVersion",
+                UnsubscribeVersion => "UnsubscribeVersion",
+                BurnAsset(_) => "BurnAsset",
+                ExpectAsset(_) => "ExpectAsset",
+                ExpectOrigin(_) => "ExpectOrigin",
+                ExpectError(_) => "ExpectError",
+                ExpectTransactStatus(_) => "ExpectTransactStatus",
+                QueryPallet { .. } => "QueryPallet",
+                ExpectPallet { .. } => "ExpectPallet",
+                ReportTransactStatus(_) => "ReportTransactStatus",
+                ClearTransactStatus => "ClearTransactStatus",
+                UniversalOrigin(_) => "UniversalOrigin",
+                ExportMessage { .. } => "ExportMessage",
+                LockAsset { .. } => "LockAsset",
+                UnlockAsset { .. } => "UnlockAsset",
+                NoteUnlockable { .. } => "NoteUnlockable",
+                RequestUnlock { .. } => "RequestUnlock",
+                SetFeesMode { .. } => "SetFeesMode",
+                SetTopic { .. } => "SetTopic",
+                ClearTopic => "ClearTopic",
+                AliasOrigin(_) => "AliasOrigin",
+                UnpaidExecution { .. } => "UnpaidExecution",
+            };
+            inst_str.to_string()
+        })
+        .fold("v2:".to_string(), |mut acc, inst| {
+            acc.push(',');
+            acc.push_str(&inst);
+            acc
+        });
 
     println!("{}", xcm_str);
 }
